9e7ac5e411470fc82cdbebf474c97ba59b596a73
Marcin Dudek <m.dudek@red.com.pl>
2014-02-12 14:37:52 +0100

    jQuery Foldable v.1.1

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ jquery-foldable
 ===============
 
 jQuery foldable plugin
+
+==CHANGELOG==
+
+=1.1=
+- Added the localstorage support
+- Added more options for foldable-title
+- Added API options to show/hide/toggle
--- jquery.foldable.js
+++ jquery.foldable.js
@@ -1,36 +1,93 @@
 /*
  * jquery.foldable.js (https://github.com/MythThrazz/jquery-foldable)
  * @author Marcin Dudek
- * @version 1.0
+ * @version 1.1
  *
  * Copyright (c) 2013 Marcin Dudek aka MythThrazz (https://github.com/MythThrazz)
  * The MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
-(function($) {
+(function ($, undefined) {
 
-    var defaultOptions = {
-        event: 'click', /* event which fires the folding effect */
-        hideText: 'Fold', /* folding button text */
-        showText: 'Unfold', /* unfolding button text */
-        titleWrapper: '<h2/>', /* folding block's wrapper element */
-        titleText: undefined, /* folding block's title */
-        titleSearchSelector: 'h1,h2,h3,h4,h5,h6', /* folding block's title selector */
-        defaultFold: false, /* if the blocks have to be folded initially */
-        showTime: 100, /* time of the animation showing the title of the folding block */
-        hideTime: 'fast', /* time of the animation hiding the title of the folding block */
-        animationTime: 'slow' /* animation time */
+var defaultOptions = {
+        event: 'click', /* nazwa zdarzenia odpalającego zwijanie */
+        hideText: 'Zwiń', /* tekst przycisku zwijającego */
+        showText: 'Rozwiń', /* tekst przycisku rozwijającego */
+        titleWrapper: '<h2/>', /* wrapper tytułu dla zwijalnego bloku */
+        titleText: undefined, /* tekst tytułu zwijalnego bloku */
+        titleAsHtml: false, /* czy tytul ma być wstawiony jako tekst czy html */
+        titleSearchSelector: 'h1,h2,h3,h4,h5,h6', /* selektor tytułu dla zwijalnego bloku */
+        defaultFold: false, /* czy bloki domyślnie zwinięte */
+        showTime: 100, /* czas animacji pokazującej tytuł zwijalnego bloku */
+        hideTime: 'fast', /* czas animacji ukrywającej tytuł zwijalnego bloku */
+        animationTime: 'slow' /* czas animacji slide */
     };
 
     var api = {
-        log: function() {
+        log: function () {
             console.log(this);
         },
-        toggle: function() {
-            return this.find('.foldable-row').trigger('mouseup.foldable');
+        toggle: function () {
+            var options = $(this).data('foldable').options;
+            return this.find('.foldable-row').trigger(options.event + '.foldable');
         },
-        showHide: function(object, options) {
-            var hideTime, animationTime, showTime;
+        show: function () {
+            var options = $.extend({'action': 'show'}, defaultOptions, $(this).data('foldable').options || {});
+            ;
+            return api.showHide(this.find('.foldable-row'), options, false);
+        },
+        hide: function () {
+            var options = $.extend({'action': 'hide'}, defaultOptions, $(this).data('foldable').options || {});
+            ;
+            return api.showHide(this.find('.foldable-row'), options, false);
+        },
+        setInLocalStorage: function (object, value) {
+            var id, foldableObject;
+
+            if(object.hasClass('foldable'))
+            {
+                foldableObject = object;
+            }
+            else
+            {
+                foldableObject = object.parents('.foldable');
+            }
+            id = (foldableObject) ? foldableObject.attr('id') : null;
+            if(localStorage && typeof id === 'string' && typeof value !== 'undefined')
+            {
+                localStorage[id] = value;
+            }
+            else
+            {
+                return null;
+            }
+        },
+        getFromLocalStorage: function (object) {
+            var id, foldableObject;
+
+            if(object.hasClass('foldable'))
+            {
+                foldableObject = object;
+            }
+            else
+            {
+                foldableObject = object.parents('.foldable');
+            }
+            id = (foldableObject) ? foldableObject.attr('id') : null;
+            if(localStorage && typeof id === 'string')
+            {
+                return localStorage[id];
+            }
+            else
+            {
+                return null;
+            }
+        },
+        showHide: function (object, options, returnValue) {
+            var hideTime, animationTime, showTime, $object;
+
+            $object = $(object);
+
             if (options.quickHide)
             {
                 hideTime = 0;
@@ -44,30 +101,43 @@
                 animationTime = options.animationTime;
             }
 
-            var $elementToFoldUnfold = $(object).siblings('.foldable-wrapper');
-            var $btn = $(object).find('.foldable-button');
-            var $title = $(object).find(options.titleSearchSelector);
+            var action = (options.action) ? options.action : 'toggle';
 
-            if (!$elementToFoldUnfold.is(':visible')) {
-                $(object).addClass('unfolded');
-                $(object).removeClass('folded');
+            var $elementToFoldUnfold = $object.siblings('.foldable-wrapper');
+            var $btn = $object.find('.foldable-button');
+            var $title = $object.find(options.titleSearchSelector);
+
+            /*
+             * Unfold
+             */
+            if ((action === 'toggle' && !$elementToFoldUnfold.is(':visible')) || action === 'show')
+            {
+                api.setInLocalStorage($object,'unfolded');
+                $object.addClass('unfolded');
+                $object.removeClass('folded');
                 $title.hide(hideTime);
                 $elementToFoldUnfold.slideDown(animationTime);
                 $btn.text(options.hideText);
                 $btn.addClass('on');
-            } else {
-                $(object).addClass('folded');
-                $(object).removeClass('unfolded');
-                $elementToFoldUnfold.slideUp(animationTime, function() {
+            }
+            /*
+             * Fold
+             */
+            else if ((action === 'toggle' && $elementToFoldUnfold.is(':visible')) || action === 'hide')
+            {
+                api.setInLocalStorage($object,'folded');
+                $object.addClass('folded');
+                $object.removeClass('unfolded');
+                $elementToFoldUnfold.slideUp(animationTime, function () {
                     $title.show(showTime);
                 });
                 $btn.text(options.showText);
                 $btn.removeClass('on');
             }
 
-            return false;
+            return returnValue;
         },
-        init: function(options) {
+        init: function (options) {
 
             var $this = $(this), data = $this.data('foldable');
             options = $.extend({}, defaultOptions, options || {});
@@ -83,12 +153,14 @@
                 });
             }
 
-            return this.each(function() {
-                var $foldableTitleText;
+            return this.each(function () {
+                var $foldableTitleText, localStorageSetting;
                 var $foldable = this;
                 var $foldableWrapper = $('<div class="foldable-wrapper" />');
                 var $foldableRow = $('<div class="foldable-row unfolded" />');
-                var $button = $('<span class="btn-small foldable-button pull-right on">' + options.hideText + '</button>');
+                var $button = $('<span class="btn-small foldable-button screen-only pull-right on">' + options.hideText + '</button>');
+
+                localStorageSetting = api.getFromLocalStorage($($foldable));
 
                 $($foldableRow).append($button);
                 $($foldable).children().wrapAll($foldableWrapper);
@@ -116,28 +188,55 @@
                 if ($foldableTitleText)
                 {
                     var $foldableTitle = $(options.titleWrapper, {
-                        text: $foldableTitleText,
                         "class": 'foldable-title'
                     });
+
+                    if (options.titleAsHtml)
+                    {
+                        if (typeof $foldableTitleText === 'object')
+                        {
+                            $foldableTitleText.detach();
+                            $foldableTitle.append($foldableTitleText.html());
+                        }
+                        else if (typeof $foldableTitleText === 'string')
+                        {
+                        $foldableTitle.html($foldableTitleText);
+                    }
+                    }
+                    else
+                    {
+                        $foldableTitle.text($foldableTitleText);
+                    }
 
                     $($foldableTitle).hide();
                     $foldableRow.append($foldableTitle);
                 }
 
-                $foldableRow.on(options.event + '.foldable', function() {
-                    api.showHide($foldableRow, options);
+                $($foldable).on(options.event + '.foldable', '.foldable-row', function (e) {
+                    api.currentEvent = e;
+                    var $target = $(e.target), returnValue = true;
+
+                    /*
+                     * If the clicked element has one of the following classes we don't want the event to bubble
+                     */
+                    if ($target.hasClass('foldable-row') || $target.hasClass('foldable-title') || $target.hasClass('foldable-button') || $target.hasClass('foldable-item'))
+                    {
+                        returnValue = false;
+                    }
+                    api.showHide($foldableRow, options, returnValue);
+
                 });
 
-                if (options.defaultFold)
+                if (options.defaultFold && localStorageSetting !== 'unfolded')
                 {
                     api.showHide($foldableRow, quickOptions);
                 }
             });
         },
-        destroy: function() {
-            return this.each(function() {
+        destroy: function () {
+            return this.each(function () {
                 var $this = $(this),
-                        data = $this.data('foldable');
+                    data = $this.data('foldable');
 
                 var options = data.options;
 
@@ -154,7 +253,7 @@
             });
         }
     };
-    $.fn.foldable = function(options) {
+    $.fn.foldable = function (options) {
         // Method calling logic
         if (api[options]) {
             return api[options].apply(this, Array.prototype.slice.call(arguments, 1));
